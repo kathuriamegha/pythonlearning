1b6478bd1207203c754d9df245a385ef3cd3bc56
kathuriamegha <kathuriamegha09@gmail.com>
2026-01-07 11:16:11 +0530

    Delete Week5/app/schemas/database.py

--- Week5/app/schemas/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://megha@localhost:5432/doctor_appointment"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
